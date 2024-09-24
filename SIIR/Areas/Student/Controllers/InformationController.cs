7660592b9770d2d9172aad243ab333cd25d12721
OrtizAxel <l20140939@queretaro.tecnm.mx>
2024-09-23 20:25:53 -0600

    Vista y Controlador de información del estudiante

---
+++ SIIR/Areas/Student/Controllers/InformationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SIIR.Areas.Student.Controllers
+{
+    public class InformationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
