6f5be7e3a27b79d585985e22d9c8dab9d9df453f
Miguel Olvera <l20140932@queretaro.tecnm.mx>
2024-09-29 09:48:04 -0600

    actualizacion a teams

--- SIIR/Areas/Admin/Controllers/TeamsController.cs
+++ SIIR/Areas/Admin/Controllers/TeamsController.cs
@@ -82,8 +82,104 @@ namespace SIIR.Areas.Admin.Controllers
             return View(teamVM);
         }
 
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            TeamVM teamVM = new()
+            {
+                Team = new(),
+                RepresentativeList = _contenedorTrabajo.Representative.GetRepresentativesList(),
+                CoachList = _contenedorTrabajo.Coach.GetCoachesList(),
+                StudentList = _contenedorTrabajo.Student.GetStudentsList()
+            };
+            if (id != null)
+            {
+                teamVM.Team = _contenedorTrabajo.Team.GetById(id.GetValueOrDefault());
+            }
+            return View(teamVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(TeamVM teamVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                string webRootPath = _hostingEnvironment.WebRootPath;
+                var files = HttpContext.Request.Form.Files;
+                var teamFromDb = _contenedorTrabajo.Team.GetById(teamVM.Team.Id);
+
+                if (files.Count() > 0)
+                {
+                    // Editar Imagen
+                    string fileName = Guid.NewGuid().ToString();
+                    var uploads = Path.Combine(webRootPath, @"images\teams");
+                    var extension = Path.GetExtension(files[0].FileName);
+
+                    var extension_new = Path.GetExtension(files[0].FileName);
+                    var imagePath = Path.Combine(webRootPath, teamFromDb.ImageUrl.TrimStart('\\'));
+
+                    if (System.IO.File.Exists(imagePath))
+                    {
+                        System.IO.File.Delete(imagePath);
+                    }
+
+                    // Subir nueva imagen
+                    using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension_new), FileMode.Create))
+                    {
+                        files[0].CopyTo(fileStreams);
+                    }
+
+                    teamVM.Team.ImageUrl = @"\images\teams\" + fileName + extension_new;
+                    _contenedorTrabajo.Team.Update(teamVM.Team);
+                    _contenedorTrabajo.Save();
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    // No se seleccionó una nueva imagen
+                    teamVM.Team.ImageUrl = teamFromDb.ImageUrl;
+                }
+
+                _contenedorTrabajo.Team.Update(teamVM.Team);
+                _contenedorTrabajo.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            teamVM.RepresentativeList = _contenedorTrabajo.Representative.GetRepresentativesList();
+            teamVM.CoachList = _contenedorTrabajo.Coach.GetCoachesList();
+            teamVM.StudentList = _contenedorTrabajo.Student.GetStudentsList();
+            return View(teamVM);
+        }
+
         #region API CALLS
 
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _contenedorTrabajo.Team.GetAll(includeProperties: "Representative,Coach,Student") });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _contenedorTrabajo.Team.GetById(id);
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            var imagePath = Path.Combine(webRootPath, objFromDb.ImageUrl.TrimStart('\\'));
+
+            if (System.IO.File.Exists(imagePath))
+            {
+                System.IO.File.Delete(imagePath);
+            }
+
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error al borrar el equipo" });
+            }
+
+            _contenedorTrabajo.Team.Remove(objFromDb);
+            _contenedorTrabajo.Save();
+            return Json(new { success = true, message = "Equipo borrado exitosamente" });
+        }
 
         #endregion
     }
---
+++ SIIR/wwwroot/js/teams.js
@@ -0,0 +1,91 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    cargarDatatable();
+});
+
+function cargarDatatable() {
+    dataTable = $("#tblTeams").DataTable({
+        "ajax": {
+            "url": "/admin/teams/GetAll",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "id", "width": "5%" },
+            { "data": "nombre", "width": "15%" },
+            { "data": "categoria", "width": "10%" },
+            {
+                "data": "urlImagen",
+                "render": function (imagen) {
+                    return `<img src="../${imagen}" width = 150"/>`
+                }
+            },
+            { "data": "coach.name", "width": "10%" },
+            { "data": "representative.name", "width": "10%" },
+            { "data": "student.name", "width": "10%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                                <a href="/Admin/Teams/Edit/${data}" class="btn btn-success text-white" style="cursor:pointer; width:140px;">
+                                <i class="far fa-edit"></i> Editar
+                                </a>
+                                &nbsp;
+                                <a onclick=Delete("/Admin/Teams/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer; width:140px;">
+                                <i class="far fa-trash-alt"></i> Borrar
+                                </a>
+                          </div>
+                         `;
+                }, "width": "25%"
+            }
+        ],
+        "language": {
+            "decimal": "",
+            "emptyTable": "No hay registros",
+            "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+            "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+            "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+            "infoPostFix": "",
+            "thousands": ",",
+            "lengthMenu": "Mostrar _MENU_ Entradas",
+            "loadingRecords": "Cargando...",
+            "processing": "Procesando...",
+            "search": "Buscar:",
+            "zeroRecords": "Sin resultados encontrados",
+            "paginate": {
+                "first": "Primero",
+                "last": "Ultimo",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            }
+        },
+        "width": "100%"
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Esta seguro de borrar?",
+        text: "Este contenido no se puede recuperar!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Si, borrar!",
+        closeOnconfirm: true
+    }, function () {
+        $.ajax({
+            type: 'DELETE',
+            url: url,
+            success: function (data) {
+                if (data.success) {
+                    toastr.success(data.message);
+                    dataTable.ajax.reload();
+                }
+                else {
+                    toastr.error(data.message);
+                }
+            }
+        });
+    });
+}
