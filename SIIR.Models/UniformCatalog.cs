95decd7cf15efec4b08e26d3b2169dd284ea1c63
CarlosRT1411 <l20140951@queretaro.tecnm.mx>
2024-09-29 20:00:41 -0600

    Bug resuelvo - uniforme podía recibir valores nulos

--- SIIR.Models/UniformCatalog.cs
+++ SIIR.Models/UniformCatalog.cs
@@ -12,10 +12,15 @@ namespace SIIR.Models
     {
         [Key]
         public int Id { get; set; }
+        [Display(Name="Nombre")]
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        [StringLength(50, ErrorMessage = "El nombre no puede exceder los 50 caracteres.")]
         public string Name { get; set; }
-        public bool? HasNumber { get; set; }
-
-        public int? RepresentativeId { get; set; }
+        [Display(Name = "¿Tiene número?")]
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        public bool HasNumber { get; set; }
+        [Required(ErrorMessage = "Debe seleccionar un grupo representativo.")]
+        public int RepresentativeId { get; set; }
         [ForeignKey("RepresentativeId")]
         public Representative? Representative { get; set; }
     }
