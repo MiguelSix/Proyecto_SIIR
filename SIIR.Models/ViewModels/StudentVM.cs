a8a5ed86969c2e50cbc8cb2da42948457a684639
MiguelSix <l20140932@queretaro.tecnm.mx>
2024-10-27 09:46:10 -0600

    Dashboard de estudiante completado, carga la info dinamicamente e igual ya jala el edit (falta ver lo de uniformes)

--- SIIR.Models/ViewModels/StudentVM.cs
+++ SIIR.Models/ViewModels/StudentVM.cs
@@ -9,7 +9,16 @@ namespace SIIR.Models.ViewModels
 {
     public class StudentVM
     {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string SecondLastName { get; set; }
+        public string ControlNumber { get; set; }
+        public string Career { get; set; }
+        public string Semester { get; set; }
+        public string ImageUrl { get; set; }
         public Student Student { get; set; }
+
+        public string FullName => $"{Name} {LastName} {SecondLastName}".Trim();
         public IEnumerable<SelectListItem>? TeamList { get; set; }
     }
 }
