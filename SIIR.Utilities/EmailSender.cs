a5e413eb7c80ed105ac7cea208b8bb4bae9454f9
MiguelSix <l20140932@queretaro.tecnm.mx>
2024-10-27 21:07:08 -0600

    Funcionalidad de recuperar contraseña completada (Falta que isma estile las vistas)

---
+++ SIIR.Utilities/EmailSender.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIIR.Utilities
+{
+    public class EmailSender : IEmailSender
+    {
+        private string _smtpServer;
+        private int _smtpPort;
+        private string _fromEmailAddress;
+        private string _fromEmailPassword;
+
+        public EmailSender(string smtpServer, int smtpPort, string fromEmailAddress, string fromEmailPassword)
+        {
+            _smtpServer = smtpServer;
+            _smtpPort = smtpPort;
+            _fromEmailAddress = fromEmailAddress;
+            _fromEmailPassword = fromEmailPassword;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string htmlMessage)
+        {
+            var smtp = new SmtpClient
+            {
+                Host = _smtpServer,
+                Port = _smtpPort,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(_fromEmailAddress, _fromEmailPassword)
+            };
+
+            using (var message = new MailMessage(_fromEmailAddress, toEmail))
+            {
+                message.Subject = subject;
+                message.Body = htmlMessage;
+                message.IsBodyHtml = true;
+                await smtp.SendMailAsync(message);
+            }
+        }
+    }
+}
