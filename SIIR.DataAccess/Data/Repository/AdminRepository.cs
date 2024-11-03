bd04c2b2d02092152f0650436829bfae4274b300
AxelOrtizRicalde <l20140939@queretaro.tecnm.mx>
2024-11-03 17:32:02 -0600

    Vista para visualizar administradores

---
+++ SIIR.DataAccess/Data/Repository/AdminRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using SIIR.Data;
+using SIIR.DataAccess.Data.Repository.IRepository;
+using SIIR.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIIR.DataAccess.Data.Repository
+{
+    public class AdminRepository: Repository<Admin>, IAdminRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public AdminRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<SelectListItem> GetAdminsList()
+        {
+            return _db.Admins.Select(i => new SelectListItem()
+            {
+                Text = i.Name + " " + i.LastName + " " + i.SecondLastName,
+                Value = i.Id.ToString()
+            });
+        }
+    }
+}
