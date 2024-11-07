50b49c97ba52090e7c302576f13a561859029ff7
CarlosRT1411 <l20140951@queretaro.tecnm.mx>
2024-11-07 16:06:46 -0600

    rosterQuickActions

--- SIIR/wwwroot/js/rosterQuickActions.js
+++ SIIR/wwwroot/js/rosterQuickActions.js
@@ -6,14 +6,15 @@ let allStudents = [];
 $(document).ready(function () {
 
     $(document).on("click", "#generateCertificateBtn", function () {
-        console.log("Holaaaa");
         const teamId = $(this).data("team-id");
         loadStudentsForCertificate(teamId);
     });
 
-    //$("#btn-generar-cedula").click(function () {
-    //    generateCertificate();
-    //});
+    $(document).on("click", "#downloadStudentsInfo", function () {
+        const teamId = $(this).data("team-id");
+        downloadAllInfo(teamId);
+    });
+
 });
 
 function loadStudentsForCertificate(teamId) {
@@ -22,11 +23,9 @@ function loadStudentsForCertificate(teamId) {
         type: 'GET',
         data: { teamId: teamId },
         success: function (response) {
+            let studentsHtml = '';
             if (response.data && response.data.length > 0) {
                 allStudents = response.data;  // Guardar todos los estudiantes en la variable global
-                let studentsHtml = '';
-                $(".checkbox-div").show();
-                $('.not-found').hide();
 
                 response.data.forEach(student => {
                     const controlNumber = student.controlNumber ? student.controlNumber : '---';
@@ -36,32 +35,31 @@ function loadStudentsForCertificate(teamId) {
                         : `<img src="${defaultImage}" class="card-img-top h-100" alt="Imagen del jugador" />`;
 
                     studentsHtml += `
-        <div class="col">
-            <div class="card position-relative">
-                ${studentImage}
-                <div class="position-absolute top-0 end-0 m-2">
-                    <input type="checkbox" class="btn-check student-checkbox" id="checkbox${student.id}" autocomplete="off">
-                    <label class="btn btn-sm checkbox" for="checkbox${student.id}">
-                        <i class="fa-solid fa-check"></i>
-                    </label>
-                </div>
-                <div class="card-body p-0 text-center">
-                    <h5 class="text-center mb-0 ps-2 pe-2 text-white title fs-6 d-flex align-items-center justify-content-center">
-                        ${student.name} ${student.lastName} ${student.secondLastName}
-                    </h5>
-                    <p class="mb-0 control-number">${controlNumber}</p>
-                </div>
-            </div>
-        </div>
-    `;
+                        <div class="col">
+                            <div class="card position-relative">
+                                ${studentImage}
+                                <div class="card-body p-0 text-center">
+                                    <h5 class="text-center mb-0 ps-2 pe-2 text-white title fs-6 d-flex align-items-center justify-content-center">
+                                        ${student.name} ${student.lastName} ${student.secondLastName}
+                                    </h5>
+                                    <p class="mb-0 control-number">${controlNumber}</p>
+                                </div>
+                            </div>
+                        </div>
+                    `;
                 });
-
-
+                $('.not-found-container').empty();
                 $('.students-container').html(studentsHtml);
                 $("#certificateModal").modal('show');
             } else {
-                $('.not-found').show();
-                $(".checkbox-div").hide();
+                studentsHtml += `
+                        <div class="not-found text-center mt-4">
+                            <i class="fa-solid fa-ban fs-2" style="color: red"></i>
+                            <p class="fs-2 mb-0">No se encontraron elementos</p>
+                        </div>
+                    `;
+                $('.students-container').empty();
+                $('.not-found-container').html(studentsHtml);
                 $("#certificateModal").modal('show');
             }
         },
@@ -69,61 +67,41 @@ function loadStudentsForCertificate(teamId) {
             toastr.error('Error al cargar la lista de estudiantes');
         }
     });
+}
 
-    $('#checkboxAll').change(function () {
-        const isChecked = $(this).is(':checked');
-        $('.student-checkbox').prop('checked', isChecked).trigger('change');
-    });
+function downloadAllInfo(teamId) {
+    $.ajax({
+        url: `${downloadAllInfoUrl}?teamId=${teamId}`,
+        type: 'POST',
+        xhrFields: { responseType: 'blob' },
+        success: function (blob, status, xhr) {
+            // Obtener el nombre del archivo desde el encabezado 'Content-Disposition'
+            const disposition = xhr.getResponseHeader('Content-Disposition');
+            let fileName = "Informacion_Estudiantes.pdf"; // Nombre por defecto
 
-    $(document).on('change', '.student-checkbox', function () {
-        if (!$(this).is(':checked')) {
-            $('#checkboxAll').prop('checked', false);
-        } else {
-            const allChecked = $('.student-checkbox:checked').length === $('.student-checkbox').length;
-            $('#checkboxAll').prop('checked', allChecked);
+            if (disposition && disposition.includes('filename=')) {
+                const filenameRegex = /filename[^;=\n]*=(['"]?)([^'"\n]*)\1/;
+                const matches = filenameRegex.exec(disposition);
+                if (matches != null && matches[2]) {
+                    fileName = matches[2];
+                }
+            }
+
+            // Crear un enlace temporal para descargar el archivo
+            const link = document.createElement('a');
+            link.href = window.URL.createObjectURL(blob);
+            link.download = fileName; // Usa el nombre del archivo extraído
+            link.click();
+            window.URL.revokeObjectURL(link.href);
+
+            toastr.success("Documento descargado correctamente.");
+        },
+        error: function (xhr) {
+            if (xhr.status === 404) {
+                toastr.error("No se encontraron estudiantes en el equipo");
+            } else {
+                toastr.error("Error al generar el documento de información de los estudiantes.");
+            }
         }
     });
 }
-
-/*function generateCertificate() {
-    // Filtra los estudiantes seleccionados a partir de la lista completa almacenada
-    const teamId = $("#teamId").val();
-    const selectedStudents = allStudents.filter(student =>
-        $(`#checkbox${student.id}`).is(':checked')
-    );
-
-    if (selectedStudents.length === 0) {
-        toastr.error('No hay estudiantes seleccionados');
-        return;
-    }
-
-    const today = new Date();
-    const year = today.getFullYear();
-    const month = String(today.getMonth() + 1).padStart(2, '0'); // Mes en formato 'MM'
-    const day = String(today.getDate()).padStart(2, '0'); // Día en formato 'DD'
-    const formattedDate = `${year}-${month}-${day}`;
-    const formattedTeamName = teamName.replace(/\s+/g, '_');
-
-    // Envía los objetos de estudiantes seleccionados al controlador
-    $.ajax({
-        url: `${downloadCertificate}?teamId=${teamId}`,
-        type: 'POST',
-        contentType: 'application/json',
-        data: JSON.stringify({
-            students: selectedStudents,
-            team: teamName,
-            coach: coachTeam
-        }),
-        xhrFields: { responseType: 'blob' },
-        success: function (blob) {
-            const link = document.createElement('a');
-            link.href = window.URL.createObjectURL(blob);
-            link.download = `Cedula_${formattedTeamName}_${formattedDate}.pdf`;
-            link.click();
-            toastr.success('Cedula generada');
-        },
-        error: function (error) {
-            toastr.error('Error al generar la cedula');
-        }
-    });
-} */
