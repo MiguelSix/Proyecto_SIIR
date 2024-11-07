85f49f6ff4bac1a01673c13529720d83487ec8a7
AxelOrtizRicalde <l20140939@queretaro.tecnm.mx>
2024-11-06 20:17:17 -0600

    Correciones de la vista roster

--- SIIR/wwwroot/js/Roster.js
+++ SIIR/wwwroot/js/Roster.js
@@ -169,7 +169,7 @@ function initializeDataTable(teamId) {
                                 <a onclick=Lock("/Admin/Students/Lock/${data.id}") class="btn btn-danger btn-sm" style="cursor:pointer;">
                                     <i class="fas fa-user-minus"></i>
                                 </a>
-                                <a onclick=downloadInfo("/Admin/Teams/GenerateStudentCertificate/${data.id}") class="btn btn-info btn-sm" style="cursor:pointer;">
+                                <a onclick=downloadInfo("/Admin/Teams/GenerateStudentCertificate/${data.id}") class="btn btn-info btn-sm" style="cursor:pointer; color:white;">
                                     <i class="fas fa-download"></i>
                                 </a>
                                 <button class="btn btn-secondary btn-sm" onclick="descargarDocs(${data.id})">
